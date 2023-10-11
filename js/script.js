8be5e220d140c2a643017979ad99330d23e62061
gitsamk <skrame18@gmail.com>
2023-10-11 13:17:09 -0700

    autoplay changes

--- js/script.js
+++ js/script.js
@@ -1,106 +1,357 @@
 document.addEventListener('DOMContentLoaded', init);
+var myInterval = setInterval(showNewSlides, 5000);
 
-
+// prepare for interaction
 function init() {
+    
+    
+
+    const container = document.querySelector('.pictures');
+
+    const containertwo = document.querySelector('.gallery');
+
+    const slideshow = document.querySelector(".slideshow");
+
+    const presentation = slideshow.querySelectorAll("img");
+
+    const slideshowtwo = document.querySelector(".slideshowtwo");
+
+    const presentationtwo = slideshowtwo.querySelectorAll("img");
+
+    const show = container.querySelectorAll("figure");
+    //console.log(presentation);
+
+    // createslide controls
+
+
+    const controlsDiv = document.createElement("div");
+
+    controlsDiv.className = 'controls';
+
+    controlsDiv.innerHTML = '<button><a href="#" class="backbutton">Back</a></button><button><a href="#" class="nextbutton">Next</a></button>';
+
+
+    //add controls to page
+
+    container.append(controlsDiv);
+
+
+    const backBtn = controlsDiv.querySelector(".backbutton");
+    const nextBtn = controlsDiv.querySelector(".nextbutton");
+
+    //add interactivity 
+
+    backBtn.addEventListener('click', showNewSlide);
+    nextBtn.addEventListener('click', showNewSlide);
 
     
-  const nextbtn = document.querySelector(".nextbutton");
-  const frame = document.querySelector(".frame");
-  const backbtn = document.querySelector(".backbutton");
-  const presentation = frame.querySelectorAll("img");
+    
+    const controlsDivTwo = document.createElement("div");
 
- 
-  
- 
-  
+    controlsDivTwo.className = 'controlstwo';
 
-  
-  
-  
-  presentation.forEach((pic) => {
-    pic.classList.add("hide");
-  });
-  
- 
-  presentation[0].classList.remove("hide");
+    controlsDivTwo.innerHTML = '<button><a href="#" class="backbuttontwo">Back</a></button><button><a href="#" class="nextbuttontwo">Next</a></button>';
 
-  
-  
-  
+
+    //add controls to page
+
+    containertwo.append(controlsDivTwo);
+
+
+    const backBtnTwo = controlsDivTwo.querySelector(".backbuttontwo");
+    const nextBtnTwo = controlsDivTwo.querySelector(".nextbuttontwo");
+
+    //add interactivity 
+    backBtnTwo.addEventListener('click', newShow);
+    nextBtnTwo.addEventListener('click', newShow);
+
+
+    
+    
+    
+    //setup slides, first hide slides except first one
+
+    presentation.forEach((pic) => {
+
+        pic.classList.add("hide");
+        presentation[0].classList.remove("hide");
+
+    });
+
+    
+
+    show.forEach((figure) => {
+
+        figure.classList.add("hide");
+        show[0].classList.remove("hide");
+
+    });
+
+
+    presentationtwo.forEach((pictwo) => {
+
+        pictwo.classList.add("hide");
+        presentationtwo[0].classList.remove("hide");
+
+    });
+    
+    backBtn.addEventListener('click', showNewSlides);
+    nextBtn.addEventListener('click', showNewSlides);
+
+    
+     //let currentImageCounter = 0;
+
+     //nextUp[currentImageCounter].style.opacity = 1;
 
    
 
-
-   nextbtn.addEventListener("click",switchpic);
-   backbtn.addEventListener("click", switchpic);
-
-
-   
-
-  
 }
 
-const controlsDiv = document.createElement('div');
-   controlsDiv.className = 'controls';
-   
-   const backBtn = document.createElement('a');
-   backBtn.href = '#';
-   backBtn.className = 'backutton';
-   backBtn.innerText = 'Back';
-   
-   const nextBtn = document.createElement('a');
-   nextBtn.href = '#';
-   nextBtn.className = 'nextbutton';
-   nextBtn.innerText = 'Next';
-   
-   controlsDiv.appendChild(backBtn);
-   controlsDiv.appendChild(nextBtn);
- 
-   const figure = document.querySelector('figure');
-   figure.insertAdjacentElement('afterend', div);
 
 
 
 
-function switchpic(e) {
-  
-   
+
+function showNewSlide(e) {
+
     e.preventDefault();
-    
-  
-    const frame = document.querySelector(".frame");
-    const presentation = frame.querySelectorAll("img");
-    let now = document.querySelector(".now");
-    let nextUp = "";
 
-    
-  
+    const controlsDiv = document.querySelector(".controls");
+    const currentSlide = document.querySelector('.now');
+    const container = document.querySelector('.pictures');
+    const backBtn = controlsDiv.querySelector(".backbutton");
+    const nextBtn = controlsDiv.querySelector(".nextbutton");
+    const slideshow = document.querySelector(".slideshow");
+
+    const presentation = slideshow.querySelectorAll("img");
+
    
-    if(e.target.className == 'nextbutton') {
-      nextUp = now.nextElementSibling;
-    }
-  
-    if(e.target.className == 'backbutton') {
-      nextUp = now.previousElementSibling;
-    }
-    
-  
-    now.classList.toggle("hide");
-    now.classList.toggle("now");
-    
-    
-    if (!nextUp) {
-      nextUp = presentation[presentation.length - 1];
-    }
-  
-   
-    if (nextUp.nodeName !== "IMG") {
-      nextUp = presentation[0];
-    }
-  
-   
-    nextUp.classList.remove("hide");
-    nextUp.classList.add("now");
-  }
 
 
+    //checkto see which button was clicked
+    const myButton = e.target;
+    //const showing =
+    let nextUp = ""
+
+    if (myButton.classList.contains('backbutton')) {
+        nextUp = currentSlide.previousElementSibling;
+    } else {
+        nextUp = currentSlide.nextElementSibling;
+    }
+
+
+
+    if (nextUp === null) {
+        let index = presentation.length - 1;
+        nextUp = presentation[index];
+    }
+
+    //nextbutton logic
+
+
+    //console.log(nextUp.nodeName);
+    if (nextUp.nodeName == 'FIGCAPTION') {
+        nextUp = presentation[0];
+
+    }
+
+    currentSlide.classList.toggle('hide');
+    currentSlide.classList.toggle('now');
+
+    nextUp.classList.toggle('hide');
+    nextUp.classList.toggle('now');
+
+
+    
+
+    //changecaption
+
+    
+    
+
+    const txt = nextUp.alt;
+    const caption = slideshow.querySelector('Figcaption');
+    caption.textContent = txt;
+
+    
+    
+
+
+
+    nextBtn.addEventListener("click", showNewSlide);
+    backBtn.addEventListener("click", showNewSlide);
+
+    
+
+    
+
+  
+
+
+}
+
+
+    function newShow(y) {
+
+    y.preventDefault(); 
+
+    const controlsDivTwo = document.querySelector(".controlstwo");
+    const currentShow = document.querySelector('.nowtwo');
+    const containertwo = document.querySelector('.gallery');
+    const backBtnTwo = controlsDivTwo.querySelector(".backbuttontwo");
+    const nextBtnTwo = controlsDivTwo.querySelector(".nextbuttontwo");
+    const container = document.querySelector(".pictures");
+
+    const show = container.querySelectorAll("figure");
+
+   
+
+
+    //checkto see which button was clicked
+    const myButtonTwo = y.target;
+    //const showing =
+    let nextUpTwo = ""
+
+    if (myButtonTwo.classList.contains('backbuttontwo')) {
+        nextUpTwo = currentShow.previousElementSibling;
+    } else {
+        nextUpTwo = currentShow.nextElementSibling;
+    }
+
+
+
+    if (nextUpTwo === null) {
+        let index = show.length - 1;
+        nextUpTwo = show[index];
+    }
+
+    if (nextUpTwo.nodeName == 'P') {
+        nextUpTwo = show[0];
+
+    }
+
+    //nextbutton logic
+
+
+    //console.log(nextUp.nodeName);
+    
+
+    currentShow.classList.toggle('hide');
+    currentShow.classList.toggle('nowtwo');
+
+    nextUpTwo.classList.toggle('hide');
+    nextUpTwo.classList.toggle('nowtwo');
+
+
+    
+
+    //changecaption
+
+    
+    
+
+    nextBtnTwo.addEventListener("click", newShow);
+    backBtnTwo.addEventListener("click", newShow);
+
+    console.log (nextUpTwo);
+
+  
+
+
+}
+
+function showNewSlides(e) {
+    if (e){
+        e.preventDefault();
+        window.clearInterval(myInterval);
+    }
+    
+
+    const controlsDiv = document.querySelector(".controls");
+    const currentSlideTwo = document.querySelector('.nowslidetwo');
+    const container = document.querySelector('.pictures');
+    const backBtn = controlsDiv.querySelector(".backbutton");
+    const nextBtn = controlsDiv.querySelector(".nextbutton");
+    const slideshowtwo = document.querySelector(".slideshowtwo");
+
+    const presentationtwo = slideshowtwo.querySelectorAll("img");
+
+   
+
+
+    //checkto see which button was clicked
+    if(e){
+        
+    const myButton = e.target;
+    console.log("test", e);
+    }
+    //const showing =
+    let nextUpOne = ""
+
+    if (!e || myButton.classList.contains('backbutton')) {
+        nextUpOne = currentSlideTwo.previousElementSibling;
+    } else {
+        nextUpOne = currentSlideTwo.nextElementSibling;
+    }
+
+
+
+    if (nextUpOne === null) {
+        let index = presentationtwo.length - 1;
+        nextUpOne = presentationtwo[index];
+    }
+
+    //nextbutton logic
+
+
+    //console.log(nextUp.nodeName);
+    if (nextUpOne.nodeName == 'FIGCAPTION') {
+        nextUpOne = presentationtwo[0];
+
+    }
+
+    currentSlideTwo.classList.toggle('hide');
+    currentSlideTwo.classList.toggle('nowslidetwo');
+
+    nextUpOne.classList.toggle('hide');
+    nextUpOne.classList.toggle('nowslidetwo');
+
+
+    
+
+    //changecaption
+    const txt = nextUpOne.alt;
+    const caption = slideshowtwo.querySelector('figcaption');
+    caption.textContent = txt;
+
+
+
+    //nextBtn.addEventListener("click", showNewSlides);
+    //backBtn.addEventListener("click", showNewSlides);
+
+    
+
+    console.log (nextUpOne,nextUpOne.previousElementSibling);
+
+  
+
+
+}
+
+
+//AUTOPLAY
+
+//const nextImageDelay = 5000;
+//let currentImageCounter = 0;
+
+//nextUp[currentImageCounter].style.opacity = 1;
+
+//setInterval(nextimage, nextImageDelay);
+
+function nextImage(index) {
+    nextUp[i].style.opacity = 0;
+
+    //currentImageCounter = (currentImageCounter+1) % presentation.length;
+
+    console.log (index);
+}
